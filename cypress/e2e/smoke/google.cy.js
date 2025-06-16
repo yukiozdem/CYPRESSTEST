de1bb4990c2ffc8f367a7bc2d65db778cd10fb08
yukiozdem <yukiozdem@gmail.com>
2025-06-16 09:13:12 -0400

    changes

---
+++ cypress/e2e/smoke/google.cy.js
@@ -0,0 +1,12 @@
+
+
+    describe("Google Search Test", () => {
+      it("should perform a search on Google",{ tags: '@smoke' }, () => {
+        // Visit Google
+        cy.visit("https://www.google.com");
+        cy.title().should('eq', 'Google')
+        // this is for PR
+        // this is for dev branch
+      });
+    })
+
